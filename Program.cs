73553128e4ed0112894dc9b56d422e8d000a3715
Konon <alenikon@mail.ru>
2023-02-02 02:19:56 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+Console.Clear();
+int[,] spiral = new int[7, 7];
+int plusnumber = 1;
+int i = 0;
+int j = 0;
+
+while (plusnumber <= spiral.GetLength(0) * spiral.GetLength(1))
+{
+  spiral[i, j] = plusnumber;
+  plusnumber++;
+  if (i <= j + 1 && i + j < spiral.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= spiral.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > spiral.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+Console.WriteLine("Квадратная спиральная матрица");
+PrintArray(spiral);
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
